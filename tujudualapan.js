fe43efc8b3b6eee0e8afa9e77881e928809801a4
lunarwar2 <101807623+lunarwar2@users.noreply.github.com>
2022-03-17 19:22:14 +0700

    Create tujudualapan.js

---
+++ tujudualapan.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'f9a0544ce2b6700f820a5c5fc9457efe',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://snailteasesimulator.com/f9a0544ce2b6700f820a5c5fc9457efe/invoke.js"></scr' + 'ipt>');
