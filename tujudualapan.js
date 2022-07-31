c9594645adc361b6886d074fe38ea63c37934a36
lunarwar2 <101807623+lunarwar2@users.noreply.github.com>
2022-07-31 17:25:29 +0700

    Update tujudualapan.js

--- tujudualapan.js
+++ tujudualapan.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 728,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://sprinterchance.com/f9a0544ce2b6700f820a5c5fc9457efe/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://sootproclaim.com/f9a0544ce2b6700f820a5c5fc9457efe/invoke.js"></scr' + 'ipt>');
